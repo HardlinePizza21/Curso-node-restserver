8e891ba67bc247094e8c9962722d18de3d21a81a
HardlinePizza21 <121771704+HardlinePizza21@users.noreply.github.com>
2024-01-11 21:52:08 -0500

    Update server.js
    Default port updated

--- models/server.js
+++ models/server.js
@@ -7,7 +7,7 @@ class server {
 
         this.app = express()
 
-        this.port = process.env.PORT;
+        this.port = process.env.PORT || 3000;
         
         this.usuariosPath = '/api/usuarios'
 
